044c2e7efd1ae931efaa4bb99eb84100a29241b7
BriceAndersEvans <102760105+BriceAndersEvans@users.noreply.github.com>
2023-01-19 14:09:54 -0500

    Worked on input and Strings

---
+++ app.py
@@ -0,0 +1,57 @@
+#Receiving input
+################
+#name = input("What is your name? ")
+#print("Hi " + name)
+
+# name = input("What is your name? ")
+# favColor = input("What is your favorite color? ")
+# print(name + " has a favorite color and it is black " + favColor)
+
+#Type Conversion
+################
+
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2023 - int(birth_year)
+# print(type(age))
+# print(age)
+
+#Strings
+# course = "Anders' Python Practice"
+# print(course)
+# print(course[0])
+# print(course[-1])
+# print(course[0:3])
+# print(course[:3])
+# print(course[:])
+#
+# anotherCourse = course[:]
+# print(anotherCourse)
+#
+# course2 = 'Anders Python "Practice"'
+# print(course2)
+#
+# course3 = '''
+# Hello Reader
+#
+# Thank you for reading
+# '''
+# print(course3)
+
+#Formatted string
+#################
+
+# firstName = "John"
+# lastName = "Smith"
+# message = firstName + " [" + lastName + "] " + "is a coder"
+# message2 = f"{firstName} [{lastName}] is a coder"
+# print(message)
+# print(message2)
+
+#String methods
+###############
+course = "Anders' Python Practice"
+
+
+
+
